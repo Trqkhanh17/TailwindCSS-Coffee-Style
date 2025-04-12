d6916a1ee41975996911dd50bfc9b8f48364d5cc
Trqkhanh17 <khanhtranquoc44@gmail.com>
2025-04-12 19:11:22 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='root'>
+        <div className="content-wrapper">
+          <header>Header</header>
+          <main>
+            <div className="slide">Slider</div>
+            <div className="story">Story</div>
+            <div className="featured-mugs">Featured Mugs</div>
+            <div className="more-product"> Product</div>
+            <div className="coffee-magazine">Coffee Magazine</div>
+            <div className="lifestyle-story">lifeStyle Story</div>
+            <div className="subscribe-us">Subscribe Us</div>
+          </main>
+          <footer>footer</footer>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
